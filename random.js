62306342567799bc7b7d75b08fa9bf9adc2a380f
bloodyserbian <50502824+bloodyserbian@users.noreply.github.com>
2020-04-28 17:50:35 +0200

    Update random.js

--- random.js
+++ random.js
@@ -1,22 +1,30 @@
-var gente = ["Vero", "Guido", "Cesar", "Sergio"];
-var pics = ["vero.png", "guido.png", "cesar.png", "sergio.png"];
+let Veronica = {name: "Vero", photo: "vero.png"}
+let Cesar = {name: "Cesar", photo: "cesar.png"}
+let Guido = {name: "Guido", photo: "guido.png"}
+let Sergio = {name: "Sergio", photo: "sergio.png"}
+let allPeople = [Veronica, Cesar, Guido, Sergio]
+let participantsArray = []
 
-var charSelect = document.querySelectorAll(".char");
+const charSelect = document.querySelectorAll(".char");
 var randomButton = document.querySelector("#random");
 var ganadorDisplay = document.querySelector("#ganador-name");
 var ganadorPicDisplay = document.querySelector("#ganadorPic");
 
 randomButton.addEventListener("click", randomize);
 
-for (var i = 0;i < charSelect.length; i++){
+for (var i = 0; i < charSelect.length; i++){
 charSelect[i].addEventListener("click", select);
 }
 
 function randomize () {
-    var winner = gente[Math.floor((Math.random() * gente.length))]
-    ganadorDisplay.textContent = winner;
-    ganadorPicDisplay.src = pics[gente.indexOf(winner)]; 
-}
+    for(var i = 0; i < charSelect.length; i++){
+    if (!charSelect[i].classList.contains("grayed-out")){
+        participantsArray.push(allPeople[i])
+    }}
+    var winner = participantsArray[Math.floor((Math.random() * participantsArray.length))]
+    ganadorDisplay.textContent = participantsArray[participantsArray.indexOf(winner)].name;
+    ganadorPicDisplay.src = participantsArray[participantsArray.indexOf(winner)].photo; 
+    participantsArray = [];}
 
 function select () {
     this.classList.toggle("grayed-out");
